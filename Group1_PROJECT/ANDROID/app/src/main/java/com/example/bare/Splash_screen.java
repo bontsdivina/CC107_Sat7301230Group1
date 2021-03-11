81fc91187386393e82ee13a089cce9a6233b51f1
Oliver,Klent Justine <77771974+OwliiBear@users.noreply.github.com>
2021-03-10 23:48:49 -0800

    Design_Project

---
+++ Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Splash_screen.java
@@ -0,0 +1,23 @@
+package com.example.bare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_screen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+        public void run() {
+            Intent homeIntent = new Intent(Splash_screen.this , MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }
+    },SPLASH_TIME_OUT);
+    }
+}
