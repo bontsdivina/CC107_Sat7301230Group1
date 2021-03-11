624d98da46d11bde2800fa36a72aeb8ae07c8012
Oliver,Klent Justine <77771974+OwliiBear@users.noreply.github.com>
2021-03-11 00:18:52 -0800

    design

--- Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Home1.java
+++ Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Home1.java
@@ -2,16 +2,37 @@ package com.example.bare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Home1 extends AppCompatActivity {
+
+    private ImageButton eating,medicine,sleep,vaccine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home1);
 
+        eating =(ImageButton) findViewById(R.id.eating);
+        medicine =(ImageButton) findViewById(R.id.medicine);
+        sleep =(ImageButton) findViewById(R.id.sleep);
+        vaccine =(ImageButton) findViewById(R.id.vaccine);
+
+        eating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEating();
+            }
+        });
+    }
+    public void openEating(){
+        Intent intent = new Intent(this,Eating.class);
+        startActivity(intent);
+    }
 
 
     }
-}
