e25a10b45c29448d67471417a027931cb0f77c94
Alvin Gonzales Bumatay <79345400+ven-ven@users.noreply.github.com>
2021-03-16 23:48:20 +0800

    alvin
    login and register

--- Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/MainActivity.java
+++ Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/MainActivity.java
@@ -6,40 +6,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
-    private Button signup,home1;
 
+    private EditText etUsername, etPassword;
+    private String Username ,Password;
+    private String URL= "http://192.168.100.4/bare/login.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        signup =(Button) findViewById(R.id.signup);
-        home1 =(Button) findViewById(R.id.login);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                openSignup();
-            }
-        });
-
-        home1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Home();
-            }
-        });
+        Username = Password = "";
+        etUsername=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
     }
-    public void openSignup(){
-        Intent intent = new Intent(this,Signup.class);
+
+    public void Signup(View view) {
+        Intent intent = new Intent(MainActivity.this, Signup.class);
         startActivity(intent);
     }
 
-    public void Home(){
-        Intent intentt = new Intent(this,Home1.class);
-        startActivity(intentt);
+    public void login(View view) {
+        Username=etUsername.getText().toString();
+        Password=etPassword.getText().toString();
+        if(!Username.equals("") && !Password.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        Intent intent = new Intent(MainActivity.this, Home1.class);
+                        startActivity(intent);
+                    } else if (response.equals("failure")) {
+                        Toast.makeText(MainActivity.this, "Invalid Login ID/Password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this,error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("Username",Username);
+                    data.put("Password",Password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }else{
+            Toast.makeText(this, "Fields can not be empty!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Signup.java
+++ Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Signup.java
@@ -6,30 +6,93 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Signup extends AppCompatActivity {
-    private Button button;
+    Button cancel, btnRegister;
+    EditText etName,etPassword,etEmail,etReenterPassword;
+    String name,email,password,reenterPassword;
+    TextView tvStatus;
+    private String URL="http://192.168.100.4/bare/register.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        etName=findViewById(R.id.etName);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        etReenterPassword=findViewById(R.id.etReenterPassword);
+        tvStatus=findViewById(R.id.tvStatus);
+        btnRegister=findViewById(R.id.btnRegister);
+        cancel=(Button) findViewById(R.id.cancel);
+        name=email=password=reenterPassword="";
 
-
-        button =(Button) findViewById(R.id.cancel);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mainact();
-
-            }
-        });
 
 
     }
 
-    public void mainact(){
-        Intent intent = new Intent(this,MainActivity.class);
+
+    public void Register(View view) {
+        name=etName.getText().toString().trim();
+        password=etPassword.getText().toString().trim();
+        email=etEmail.getText().toString().trim();
+        reenterPassword=etReenterPassword.getText().toString().trim();
+        if (!password.equals(reenterPassword)){
+            Toast.makeText(this,"Password Mismatch Make Sure You Enter Correct Password",Toast.LENGTH_SHORT).show();
+        }
+        else if(!name.equals("") && !email.equals("") && !password.equals(""));
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("success")) {
+                    tvStatus.setText("Successfully Registered.");
+                    etName.setText("");
+                    etEmail.setText("");
+                    etPassword.setText("");
+                    etReenterPassword.setText("");
+                    btnRegister.setClickable(false);
+
+                } else if (response.equals("failure")) {
+                    tvStatus.setText("Something went wrong!");
+                    btnRegister.setClickable(false);
+                }
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("Username",name);
+                data.put("Password",password);
+                data.put("Email",email);
+                return data;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+
+
+    public void cancel(View view) {
+        Intent intent=new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
