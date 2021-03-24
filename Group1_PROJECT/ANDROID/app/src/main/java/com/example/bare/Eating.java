b811c7aae413dbbdedcdba89914ced39483f479d
Oliver,Klent Justine <77771974+OwliiBear@users.noreply.github.com>
2021-03-24 01:55:17 -0700

    resize

--- Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Eating.java
+++ Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Eating.java
@@ -2,7 +2,10 @@ package com.example.bare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Eating extends AppCompatActivity {
 
