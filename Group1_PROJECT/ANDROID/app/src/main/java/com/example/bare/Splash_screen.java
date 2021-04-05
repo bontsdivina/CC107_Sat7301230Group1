aec5bea3c944a76462967c9edc2ae2979ad2af90
Oliver,Klent Justine <77771974+OwliiBear@users.noreply.github.com>
2021-04-04 19:32:33 -0700

    splashscreen

--- Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Splash_screen.java
+++ Group1_PROJECT/ANDROID/app/src/main/java/com/example/bare/Splash_screen.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash_screen extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        imageView=(ImageView)findViewById(R.id.imageView);
         new Handler().postDelayed(new Runnable() {
         public void run() {
             Intent homeIntent = new Intent(Splash_screen.this , MainActivity.class);
@@ -19,5 +24,10 @@ public class Splash_screen extends AppCompatActivity {
             finish();
         }
     },SPLASH_TIME_OUT);
+
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.splash);
+        imageView.startAnimation(myanim);
     }
-}
+
+    }
